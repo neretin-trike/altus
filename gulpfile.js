0869cb3f296b8bd5db6254aa8d66855946a03d9c
Artem Neretin <hawktrike@gmail.com>
2019-03-11 14:24:33 +0700

    update(html): вёрстка header и секции home

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,12 @@ const config = {
 };
 
 gulp.task('css', function() {
-    console.log("шо");
 	return gulp.src('src/**/*.css')
 	    .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('html', function() {
+    console.log("шо");
     return gulp.src('src/*.html')
 	    .pipe(browserSync.reload({ stream: true }))
 });
@@ -24,7 +24,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/*.html"', gulp.parallel('html')); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('src/*.html', gulp.parallel('html')); // Наблюдение за HTML файлами в корне проекта
     gulp.watch('src/**/*.css', gulp.parallel('css')); // Наблюдение за css файлами
 });
 gulp.task('default', gulp.parallel('browser-sync', 'watch'));
